4177dd74abd29f60e645f59a3764f431c2fa049c
crystal <crystalarcus@gmail.com>
2026-07-21 12:20:16 +0530

    Lab 7

---
+++ lab7/sktech.ino
@@ -0,0 +1,20 @@
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  if (!IMU.begin()) while (1);
+}
+
+void loop() {
+  float x, y, z;
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    if (x > 0.5) Serial.println("Right");
+    else if (x < -0.5) Serial.println("Left");
+    else if (y > 0.5) Serial.println("Front");
+    else if (y < -0.5) Serial.println("Back");
+  }
+  delay(200);
+}
